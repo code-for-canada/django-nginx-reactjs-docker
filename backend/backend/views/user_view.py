49037d7844f846d4a303ba332fd42156c5ad3766
Francis Normand <francis.normand@canada.ca>
2019-02-05 11:01:53 -0500

    Added config that allows only Get requests in user view file

--- backend/backend/views/user_view.py
+++ backend/backend/views/user_view.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
+from rest_framework import generics
 from backend.serializers.user_serializer import UserSerializer
 from backend.models.user_model import MyUser
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet, generics.RetrieveAPIView):
     # GETS USER LIST
     queryset = MyUser.objects.all() # same as 'SELECT * FROM backend_myuser;'
     serializer_class = UserSerializer
+    # allows only GET requests
+    http_method_names = ['get']
