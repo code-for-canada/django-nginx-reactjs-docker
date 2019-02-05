4e47473e59c4d74cee1407fb46e06838e9a874e6
Francis Normand <francis.normand@canada.ca>
2019-02-05 11:23:37 -0500

    Removed unwanted argument in UserViewSet class

--- backend/backend/views/user_view.py
+++ backend/backend/views/user_view.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
-from rest_framework import generics
 from backend.serializers.user_serializer import UserSerializer
 from backend.models.user_model import MyUser
 
-class UserViewSet(viewsets.ModelViewSet, generics.RetrieveAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     # GETS USER LIST
     queryset = MyUser.objects.all() # same as 'SELECT * FROM backend_myuser;'
     serializer_class = UserSerializer
