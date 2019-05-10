6600eb1a0b3d6f3d99a5a6490e811e09b3076615
Francis Normand <francis.normand@canada.ca>
2019-05-10 12:09:33 -0400

    Removed DEBUG config from base.py + Removed INTERNAL_IPS config from local.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -23,9 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "%qviowlmp*kitbai+y!%1y=jdl_o3_#7+_ud6l9uwn$$=bxt1y"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = "*"
 CORS_ORIGIN_ALLOW_ALL = True
 
