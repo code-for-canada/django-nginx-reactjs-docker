adb00f91adc6de9b890f75a11025caf25ee6eced
Francis Normand <francis.normand@canada.ca>
2019-05-14 11:01:49 -0400

    Renamed and updated development file ==> dev + Updated environment configs

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -3,14 +3,14 @@ from .base import *
 
 DEBUG = True
 
-# TODO: Setup the real dev DB here
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DATABASE_NAME", ""),
+        "USER": os.environ.get("DATABASE_USER", ""),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD", ""),
+        "HOST": os.environ.get("DATABASE_HOST", ""),
+        "PORT": os.environ.get("DATABASE_PORT", ""),
     }
 }
 
