ae40583908e98300174e316b4785abc87de91609
Francis Normand <francis.normand@canada.ca>
2019-05-14 14:02:00 -0400

    Fixed LoginRedux tests

--- frontend/src/tests/modules/LoginRedux.test.js
+++ frontend/src/tests/modules/LoginRedux.test.js
@@ -1,18 +1,16 @@
-import login, { loginAction, authenticateAction, initialState } from "../../modules/LoginRedux";
+import login, { authenticateAction, initialState } from "../../modules/LoginRedux";
 
 describe("authenticate action", () => {
   it("should update authenticated state to true", () => {
     const action = authenticateAction(true);
     expect(login(initialState, action)).toEqual({
-      authenticated: true,
-      registration_message: ""
+      authenticated: true
     });
   });
   it("should update authenticated state to false", () => {
     const action = authenticateAction(false);
     expect(login(initialState, action)).toEqual({
-      authenticated: false,
-      registration_message: ""
+      authenticated: false
     });
   });
 });
