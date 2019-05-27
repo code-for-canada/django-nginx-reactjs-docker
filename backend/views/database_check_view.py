31f2898a225b2ddda4b856166a1089b9ccd651d3
Francis Normand <francis.normand@canada.ca>
2019-05-27 14:51:50 -0400

    Reorganized imports in database_check_view

--- backend/views/database_check_view.py
+++ backend/views/database_check_view.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from serializers.database_check_serializer import DatabaseCheckSerializer
 from custom_models.database_models import DatabaseCheckModel
-from rest_framework import permissions
 
 
 class DatabaseViewSet(viewsets.ModelViewSet):
