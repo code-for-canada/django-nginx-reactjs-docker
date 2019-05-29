97c4989fa9b06d1c1f002a68fae14748181fa631
Francis Normand <francis.normand@canada.ca>
2019-05-29 11:45:50 -0400

    Refactored urls.py file - Removed unneeded code

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,12 +1,9 @@
-from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.urls import path, include
-from rest_framework import routers
-from django.conf.urls.static import static
 from django.contrib import admin
-from views import views, database_check_view
-
+from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
+from views import views, database_check_view
 
 schema_view = get_swagger_view(title="ThunderCAT APIs")
 
@@ -18,19 +15,7 @@ urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^api/auth/", include("djoser.urls")),
     url(r"^api/auth/", include("djoser.urls.authtoken")),
+    url(r"^api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(r"api/backend-status", views.index, name="index"),
     path("", include(router.urls)),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Browsable API login
-urlpatterns += [
-    url(r"^api/auth/", include("rest_framework.urls", namespace="rest_framework"))
-]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
