2e93c1fb7c3f1aff3fa3dff9fe506a499d151c96
Francis Normand <francis.normand@canada.ca>
2019-05-29 13:21:16 -0400

    Removed the database check access restriction, since this API is used in the status page

--- backend/views/database_check_view.py
+++ backend/views/database_check_view.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework import permissions
 from serializers.database_check_serializer import DatabaseCheckSerializer
 from custom_models.database_models import DatabaseCheckModel
 
@@ -8,7 +7,5 @@ class DatabaseViewSet(viewsets.ModelViewSet):
     # same as 'SELECT * FROM backend_databasecheckmodel;'
     queryset = DatabaseCheckModel.objects.all()
     serializer_class = DatabaseCheckSerializer
-    # allow admin users only to view this API
-    permission_classes = (permissions.IsAdminUser,)
     # allows only GET requests
     http_method_names = ["get"]
