5c358f2235c97f6bdbdbacf918350f38996507dc
Francis Normand <francis.normand@canada.ca>
2019-05-30 06:39:00 -0400

    Put back the JSONWebTokenAuthentication in DEFAULT_AUTHENTICATION_CLASSES configs

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -93,6 +93,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
     ),
 }
 
