e93f1a9a5fc45216c667006467b876fca2c72051
Michael Cherry <zorackiii@gmail.com>
2019-05-30 12:51:15 -0400

    Adding item creation and rollback

--- backend/custom_models/migrations/0005_upload_emib_sample.py
+++ backend/custom_models/migrations/0005_upload_emib_sample.py
@@ -40,7 +40,85 @@ def upload_emib_sample(apps, schema_editor):
     qt_email = question_type(question_type_desc="email")
     qt_email.save()
 
-    # TODO create
+    # create items; do not use bulk_create since we need these objects later on
+    # test
+    i_emib = item(item_type_id=it_test, order=1)
+    i_emib.save()
+
+    # question 1 items
+    i_q1 = item(parent_id=i_emib,
+                item_type_id=it_question, order=1)
+    i_q1.save()
+
+    i_q1_subject = item(parent_id=i_q1,
+                        item_type_id=it_subject, order=1)
+    i_q1_subject.save()
+
+    i_q1_from = item(parent_id=i_q1,
+                     item_type_id=it_from, order=2)
+    i_q1_from.save()
+
+    i_q1_to = item(parent_id=i_q1,
+                   item_type_id=it_to, order=3)
+    i_q1_to.save()
+
+    i_q1_date = item(parent_id=i_q1,
+                     item_type_id=it_date, order=4)
+    i_q1_date.save()
+
+    i_q1_body = item(parent_id=i_q1,
+                     item_type_id=it_body, order=5)
+    i_q1_body.save()
+
+    # question 2 items
+    i_q2 = item(parent_id=i_emib,
+                item_type_id=it_question, order=2)
+    i_q2.save()
+
+    i_q2_subject = item(parent_id=i_q2,
+                        item_type_id=it_subject, order=1)
+    i_q2_subject.save()
+
+    i_q2_from = item(parent_id=i_q2,
+                     item_type_id=it_from, order=2)
+    i_q2_from.save()
+
+    i_q2_to = item(parent_id=i_q2,
+                   item_type_id=it_to, order=3)
+    i_q2_to.save()
+
+    i_q2_date = item(parent_id=i_q2,
+                     item_type_id=it_date, order=4)
+    i_q2_date.save()
+
+    i_q2_body = item(parent_id=i_q2,
+                     item_type_id=it_body, order=5)
+    i_q2_body.save()
+
+    # question 3 items
+    i_q3 = item(parent_id=i_emib,
+                item_type_id=it_question, order=3)
+    i_q3.save()
+
+    i_q3_subject = item(parent_id=i_q3,
+                        item_type_id=it_subject, order=1)
+    i_q3_subject.save()
+
+    i_q3_from = item(parent_id=i_q3,
+                     item_type_id=it_from, order=2)
+    i_q3_from.save()
+
+    i_q3_to = item(parent_id=i_q3,
+                   item_type_id=it_to, order=3)
+    i_q3_to.save()
+
+    i_q3_date = item(parent_id=i_q3,
+                     item_type_id=it_date, order=4)
+    i_q3_date.save()
+
+    i_q3_body = item(parent_id=i_q3,
+                     item_type_id=it_body, order=5)
+    i_q3_body.save()
 
 
 def destroy_emi_sample(apps, schema_editor):
@@ -71,8 +149,68 @@ def destroy_emi_sample(apps, schema_editor):
     # get question_type objects
     qt_email = question_type.objects.using(
         db_alias).filter(question_type_desc="email").last()
+    # get item objects
+    i_emib = item.objects.using(db_alias).filter(
+        item_type_id=it_test, order=1).last()
+    i_q1 = item.objects.using(db_alias).filter(
+        parent_id=i_emib, item_type_id=it_question, order=1).last()
+    i_q1_subject = item.objects.using(db_alias).filter(
+        parent_id=i_q1, item_type_id=it_subject, order=1).last()
+    i_q1_from = item.objects.using(db_alias).filter(
+        parent_id=i_q1, item_type_id=it_from, order=2).last()
+    i_q1_to = item.objects.using(db_alias).filter(
+        parent_id=i_q1, item_type_id=it_to, order=3).last()
+    i_q1_date = item.objects.using(db_alias).filter(
+        parent_id=i_q1, item_type_id=it_date, order=4).last()
+    i_q1_body = item.objects.using(db_alias).filter(
+        parent_id=i_q1, item_type_id=it_body, order=5).last()
+    i_q2 = item.objects.using(db_alias).filter(
+        parent_id=i_emib, item_type_id=it_question, order=2).last()
+    i_q2_subject = item.objects.using(db_alias).filter(
+        parent_id=i_q2, item_type_id=it_subject, order=1).last()
+    i_q2_from = item.objects.using(db_alias).filter(
+        parent_id=i_q2, item_type_id=it_from, order=2).last()
+    i_q2_to = item.objects.using(db_alias).filter(
+        parent_id=i_q2, item_type_id=it_to, order=3).last()
+    i_q2_date = item.objects.using(db_alias).filter(
+        parent_id=i_q2, item_type_id=it_date, order=4).last()
+    i_q2_body = item.objects.using(db_alias).filter(
+        parent_id=i_q2, item_type_id=it_body, order=5).last()
+    i_q3 = item.objects.using(db_alias).filter(
+        parent_id=i_emib, item_type_id=it_question, order=3).last()
+    i_q3_subject = item.objects.using(db_alias).filter(
+        parent_id=i_q3, item_type_id=it_subject, order=1).last()
+    i_q3_from = item.objects.using(db_alias).filter(
+        parent_id=i_q3, item_type_id=it_from, order=2).last()
+    i_q3_to = item.objects.using(db_alias).filter(
+        parent_id=i_q3,   item_type_id=it_to, order=3).last()
+    i_q3_date = item.objects.using(db_alias).filter(
+        parent_id=i_q3, item_type_id=it_date, order=4).last()
+    i_q3_body = item.objects.using(db_alias).filter(
+        parent_id=i_q3, item_type_id=it_body, order=5).last()
 
     # TODO roll back
+
+    # destroy items; inverted order as children must be deleted first
+    i_q3_body.delete()
+    i_q3_date.delete()
+    i_q3_to.delete()
+    i_q3_from.delete()
+    i_q3_subject.delete()
+    i_q3.delete()
+    i_q2_body.delete()
+    i_q2_date.delete()
+    i_q2_to.delete()
+    i_q2_from.delete()
+    i_q2_subject.delete()
+    i_q2.delete()
+    i_q1_body.delete()
+    i_q1_date.delete()
+    i_q1_to.delete()
+    i_q1_from.delete()
+    i_q1_subject.delete()
+    i_q1.delete()
+    i_emib.delete()
 
     # destroy question_types
     qt_email.delete()
