3176cff3c1af31353290a9afb7691050f2993c4f
Francis Normand <francis.normand@canada.ca>
2019-06-03 10:25:18 -0400

    Added comments in history.js file

--- frontend/src/components/authentication/history.js
+++ frontend/src/components/authentication/history.js
@@ -1,3 +1,10 @@
+/*
+The DOM window object provides access to the browser's session history 
+through the history object. It exposes useful methods and properties that 
+let you move back and forth through the user's history.
+
+ref: https://developer.mozilla.org/en-US/docs/Web/API/History_API
+*/
 import { createBrowserHistory } from "history";
 
 export default createBrowserHistory();
