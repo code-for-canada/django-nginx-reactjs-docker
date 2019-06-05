03cdfb3f598465817a24764c771c8e6046d74222
Francis Normand <francis.normand@canada.ca>
2019-06-05 11:43:29 -0400

    Added dynamic password validation in RegexValidator.js

--- frontend/src/helpers/regexValidator.js
+++ frontend/src/helpers/regexValidator.js
@@ -21,18 +21,54 @@ export function validateEmail(string) {
 
 /*
 Conditions:
-  - At least one upper case
-  - At least one lower case
+  - At least one uppercase
+  - At least one lowercase
   - At least one digit
   - At least one special character
   - Minimum 5 and maximum 15 in length
 */
+
+export const PASSWORD_REQUIREMENTS = {
+  UPPERCASE: "uppercase",
+  LOWERCASE: "lowercase",
+  DIGIT: "digit",
+  SPECIAL_CHARS: "special_char",
+  NUMBER_OF_CHARS: "number_of_chars"
+};
+
 export function validatePassword(string) {
+  let array = [];
   const regexExpression = /^(?=.*?[A-ZÀ-ÿ])(?=.*?[a-zÀ-ÿ])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,15}$/;
   if (regexExpression.test(string)) {
-    return true;
+    // empty array
+    return array;
   } else {
-    return false;
+    const uppercaseRegex = /(?=.*[A-Z])/;
+    const lowercaseRegex = /(?=.*[a-z])/;
+    const digitRegex = /(?=.*\d)/;
+    const specialCharRegex = /(?=.*?[#?!@$%^&*-])/;
+    const numberOfCharRegex = /^(?=.{5,15}$).*/;
+    // at least one uppercase
+    if (!uppercaseRegex.test(string)) {
+      array.push(PASSWORD_REQUIREMENTS.UPPERCASE);
+    }
+    // at least one lowercase
+    if (!lowercaseRegex.test(string)) {
+      array.push(PASSWORD_REQUIREMENTS.LOWERCASE);
+    }
+    // at least one digit
+    if (!digitRegex.test(string)) {
+      array.push(PASSWORD_REQUIREMENTS.DIGIT);
+    }
+    // at least one special character
+    if (!specialCharRegex.test(string)) {
+      array.push(PASSWORD_REQUIREMENTS.SPECIAL_CHARS);
+    }
+    // minimum of 5 and maximum of 15 characters in length
+    if (!numberOfCharRegex.test(string)) {
+      array.push(PASSWORD_REQUIREMENTS.NUMBER_OF_CHARS);
+    }
+    return array;
   }
 }
 
