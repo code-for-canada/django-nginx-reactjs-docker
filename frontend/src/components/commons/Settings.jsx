7ede891b8974b7d10ddf717deeb559ea786c4c2b
Caley Brock <caleybrock7@gmail.com>
2019-06-05 16:22:17 -0400

    Add all content and structure

--- frontend/src/components/commons/Settings.jsx
+++ frontend/src/components/commons/Settings.jsx
@@ -43,13 +43,58 @@ class Settings extends Component {
           description={
             <div>
               <h3>Zoom (+/-)</h3>
-              <p>Info</p>
+              <ol>
+                <li>Select the View button at the top left bar in Internet Explorer</li>
+                <li>Select Zoom.</li>
+                <li>
+                  You can select a predefined zoom level, or a custom level by selecting Custom and
+                  entering a zoom value.
+                </li>
+                <li>
+                  Alternatively, you can hold down CTRL and the + / - keys on your keyboard to zoom
+                  in or out.
+                </li>
+              </ol>
               <h3>Text size</h3>
-              <p>Info</p>
+              <ol>
+                <li>Select the View button at the top left bar in Internet Explorer</li>
+                <li>Select Text size.</li>
+                <li>Choose to make text larger or smaller than the size on the screen.</li>
+              </ol>
+              If the text size has not changed:
+              <ol>
+                <li>
+                  Select the Tools button, and select General tab, and then, under Appearance,
+                  select Accessibility.
+                </li>
+                <li>Select the Ignore font sizes specified on webpages on the check box.</li>
+                <li>Select OK, and then select OK again.</li>
+              </ol>
               <h3>Font style</h3>
-              <p>Info</p>
+              <ol>
+                <li>Select the Tools button at the top left bar in Internet Explorer.</li>
+                <li>Select Internet options.</li>
+                <li>In the General tab, under Appearance, select Accessibility. </li>
+                <li>Select the Ignore font styles specified on webpages on the check box. </li>
+                <li>Select OK.</li>
+                <li>In the General tab, under Appearance, select Fonts.</li>
+                <li>Select the fonts you want to use.</li>
+                <li>Select OK, and then select OK again.</li>
+              </ol>
               <h3>Text and background colour</h3>
-              <p>Info</p>
+              <ol>
+                <li>Select the Tools button and select Internet options.</li>
+                <li>In the General tab, under Appearance, select Accessibility. </li>
+                <li>Select the Ignore colors specified on webpages on the check box. </li>
+                <li>Select OK.</li>
+                <li>In the General tab, under Appearance, select Colors.</li>
+                <li>Uncheck the Use Windows colors check box.</li>
+                <li>
+                  For each color that you want to change, select the color box, select a new color,
+                  and then select OK.
+                </li>
+                <li>Select OK, and then select OK again.</li>
+              </ol>
             </div>
           }
           rightButtonType={BUTTON_TYPE.primary}
