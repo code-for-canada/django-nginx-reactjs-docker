15fab01354a67988458aeb94d84e1637d2cc8553
Gabriel Jean <theelithe555@gmail.com>
2019-06-05 11:35:53 +0000

    fixed the build command in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 path="/CAT"
 
 
@@ -7,12 +7,12 @@ if [ -d $path/project-thundercat ]; then
     cd $path/project-thundercat
     git pull
     docker-compose -f docker-compose-dev.yml down
-    docker-compose -f docker-compose-dev.yml rebuild
+    docker-compose -f docker-compose-dev.yml build
     docker-compose -f docker-compose-dev.yml up -d
 else
     git clone https://github.com/code-for-canada/project-thundercat.git $path/project-thundercat
-    cd $path/project-thundercat
+    cd $path/project-thundercat 
     docker-compose -f docker-compose-dev.yml down
-    docker-compose -f docker-compose-dev.yml rebuild
+    docker-compose -f docker-compose-dev.yml build
     docker-compose -f docker-compose-dev.yml up -d
 fi
