88871fc5495ffa814a715d999341690228192e33
Michael Cherry <zorackiii@gmail.com>
2019-06-07 13:09:29 -0400

    Adding tests for pre-test api

--- backend/tests/test_retrieve_test_view.py
+++ backend/tests/test_retrieve_test_view.py
@@ -1,9 +1,9 @@
 from django.utils import timezone
 from django.test import TestCase
-from views.retrieve_test_view import retrieve_json_from_name_date, META_TEST
+from views.retrieve_test_view import retrieve_json_from_name_date, META_TEST, PRE_TEST
 
 
-class RetrieveTest(TestCase):
+class RetrieveMetaTest(TestCase):
     def test_get_real_sample(self):
         real_json = retrieve_json_from_name_date(
             "emibSampleTest", timezone.now(), META_TEST)
@@ -28,5 +28,35 @@ class RetrieveTest(TestCase):
         time = timezone.now() + timezone.timedelta(days=-1)
         real_json = retrieve_json_from_name_date(
             "emibSampleTest", time, META_TEST)
+        expected_json = {'error', 'no test item found'}
+        self.assertEqual(real_json, expected_json)
+
+
+class RetrievePreTest(TestCase):
+    def test_get_real_sample(self):
+        real_json = retrieve_json_from_name_date(
+            "emibSampleTest", timezone.now(), PRE_TEST)
+        expected_json = {
+            "test_internal_name": "emibSampleTest",
+            "test_en_name": "eMiB Sample Test",
+            "meta_test.test_fr_name": "FR eMiB Sample Test",
+            "meta_test.is_public": True,
+            "meta_test.default_time": None,
+            "meta_test.test_type": "emib",
+            "instructions": []
+        }
+        self.assertEqual(real_json, expected_json)
+
+    def test_get_nonexistant_test(self):
+        real_json = retrieve_json_from_name_date(
+            "IAmNotARealTest", timezone.now(), PRE_TEST)
+        expected_json = {"error", "no test with the given test_name"}
+        self.assertEqual(real_json, expected_json)
+
+    def test_get_test_before(self):
+        time = timezone.datetime.strptime('01/01/1500', "%d/%m/%Y").date()
+        time = timezone.now() + timezone.timedelta(days=-1)
+        real_json = retrieve_json_from_name_date(
+            "emibSampleTest", time, PRE_TEST)
         expected_json = {'error', 'no test item found'}
         self.assertEqual(real_json, expected_json)
