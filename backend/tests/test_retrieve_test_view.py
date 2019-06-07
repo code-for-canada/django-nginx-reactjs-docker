7d9d97a8755bc95418f67086d155cdfbe922497a
Michael Cherry <zorackiii@gmail.com>
2019-06-07 11:09:55 -0400

    Adding test for to get test before it was made

--- backend/tests/test_retrieve_test_view.py
+++ backend/tests/test_retrieve_test_view.py
@@ -22,3 +22,11 @@ class RetrieveTest(TestCase):
             "IAmNotARealTest", timezone.now(), META_TEST)
         expected_json = {"error", "no test with the given test_name"}
         self.assertEqual(real_json, expected_json)
+
+    def test_get_test_before(self):
+        time = timezone.datetime.strptime('01/01/1500', "%d/%m/%Y").date()
+        time = timezone.now() + timezone.timedelta(days=-1)
+        real_json = retrieve_json_from_name_date(
+            "emibSampleTest", time, META_TEST)
+        expected_json = {'error', 'no test item found'}
+        self.assertEqual(real_json, expected_json)
