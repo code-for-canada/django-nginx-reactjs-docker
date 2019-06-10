00df117404319f60467612fe85d4cb3fe3456b7b
Michael Cherry <zorackiii@gmail.com>
2019-06-10 10:21:15 -0400

    merging in create-test-api

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -30,8 +30,6 @@ urlpatterns = [
     url(r"^api/auth/jwt/verify_token/", verify_jwt_token),
     url(r"^api/test-meta-data",
         test_meta_data_view.TestMetaDataSet.as_view()),
-    url(r"^api/test-meta-data/(?P<test_name>.+)/$",
-        test_meta_data_view.TestMetaDataSet.as_view()),
     url(r"^api/test-instructions",
         test_instructions_view.TestInstructionsSet.as_view()),
     url(r"^api/test-instructions/(?P<test_name>.+)/$",
