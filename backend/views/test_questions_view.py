4a348814b2c4d130ab5cc3502bffc9b132c3cb9b
Michael Cherry <zorackiii@gmail.com>
2019-06-11 07:57:42 -0400

    fixing permissions on other two APIs

--- backend/views/test_questions_view.py
+++ backend/views/test_questions_view.py
@@ -1,13 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
-from views.retrieve_test_view import TEST_QUESTIONS, retrieve_test_data
+from views.retrieve_test_view import TEST_QUESTIONS, retrieve_test_data, is_test_public
 
 
 class TestQuestionsSet(APIView):
-    # Can only be accessed by Admin
-    permission_classes = [permissions.IsAdminUser]
-
     def get(self, request):
         return retrieve_test_data(request, TEST_QUESTIONS)
+
+    # assign the permissions depending on if the test is public or not
+    def get_permissions(self):
+        # is_public = true
+        if is_test_public(self.request.query_params.get("test_name", None)):
+            return [permissions.IsAuthenticatedOrReadOnly()]
+        # is_public = false
+        else:
+            return [permissions.IsAdminUser()]
 
 # http://localhost/api/test_questions/?test_name=emibSampleTest
