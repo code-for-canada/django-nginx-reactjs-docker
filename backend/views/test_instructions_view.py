8763534b9c86bf8bb81505f7c9697f89a9819d76
Francis Normand <francis.normand@canada.ca>
2019-06-12 08:44:48 -0400

    Fixed swagger access issues

--- backend/views/test_instructions_view.py
+++ backend/views/test_instructions_view.py
@@ -1,6 +1,11 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
 from rest_framework import permissions
-from views.retrieve_test_view import TEST_INSTRUCTIONS, retrieve_test_data, is_test_public
+from views.retrieve_test_view import (
+    TEST_INSTRUCTIONS,
+    retrieve_test_data,
+    is_test_public,
+)
 
 
 class TestInstructionsSet(APIView):
@@ -9,11 +14,16 @@ class TestInstructionsSet(APIView):
 
     # assign the permissions depending on if the test is public or not
     def get_permissions(self):
-        # is_public = true
-        if is_test_public(self.request.query_params.get("test_name", None)):
-            return [permissions.IsAuthenticatedOrReadOnly()]
-        # is_public = false
-        else:
+        try:
+            # is_public = true
+            if is_test_public(self.request.query_params.get("test_name", None)):
+                return [permissions.IsAuthenticatedOrReadOnly()]
+            # is_public = false
+            else:
+                return [permissions.IsAdminUser()]
+        # is_test_public does not exist
+        except ObjectDoesNotExist:
             return [permissions.IsAdminUser()]
+
 
 # http://localhost/api/test-instructions/?test_name=emibSampleTest
