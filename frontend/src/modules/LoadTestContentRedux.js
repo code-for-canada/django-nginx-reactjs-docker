26aedd05a1e7c74b3206619a8e2d4109090d63d2
Francis Normand <francis.normand@canada.ca>
2019-06-12 13:14:11 -0400

    Created a new redux module called 'LoadTestContentRedux' + Added test meta data functions/actions

---
+++ frontend/src/modules/LoadTestContentRedux.js
@@ -0,0 +1,39 @@
+// Action Types
+const UPDATE_TEST_META_DATA = "emibInbox/UPDATE_TEST_META_DATA";
+
+// Action Creators
+const updateTestMetaDataState = testMetaData => ({ type: UPDATE_TEST_META_DATA, testMetaData });
+
+const getTestMetaData = testName => {
+  return async function() {
+    let metaDataContent = await fetch(`/api/test-meta-data/?test_name=${testName}`, {
+      method: "GET",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      cache: "default"
+    });
+    return await metaDataContent.json();
+  };
+};
+
+// Initial State
+const initialState = {
+  testMetaData: ""
+};
+
+// Reducer
+const loadTestContent = (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_TEST_META_DATA:
+      return {
+        ...state,
+        testMetaData: action.testMetaData
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default loadTestContent;
+export { initialState, updateTestMetaDataState, getTestMetaData };
