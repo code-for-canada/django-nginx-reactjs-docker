44338a9620e2e286a2b9e21fc167e359301c1038
Francis Normand <francis.normand@canada.ca>
2019-06-14 07:17:17 -0400

    Added user's first and last name from local storage in dashboard page

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -6,7 +6,11 @@ class Dashboard extends Component {
     return (
       <div>
         <h1 className="green-divider">
-          {LOCALIZE.formatString(LOCALIZE.dashboard.title, "Current User Name Here")}
+          {LOCALIZE.formatString(
+            LOCALIZE.dashboard.title,
+            localStorage.first_name,
+            localStorage.last_name
+          )}
         </h1>
         <p>{LOCALIZE.dashboard.description}</p>
       </div>
