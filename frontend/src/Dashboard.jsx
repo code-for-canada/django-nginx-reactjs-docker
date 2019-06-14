81b8592b45d852f484061a5c5fc949ccd297aad6
Francis Normand <francis.normand@canada.ca>
2019-06-14 14:28:34 -0400

    Connected redux to Dashboard component + Added a componentDidMount function to call the new getUserInformation action + Save info in states and used these states to populate the first and last name

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import LOCALIZE from "./text_resources";
+import { connect } from "react-redux";
+import { getUserInformation } from "./modules/LoginRedux";
+import { bindActionCreators } from "redux";
 
 class Dashboard extends Component {
+  static propTypes = {
+    // Props from Redux
+    getUserInformation: PropTypes.func
+  };
+
+  state = {
+    first_name: "",
+    last_name: ""
+  };
+
+  // calling the getUserInformation on page load to get the first name and last name and save them in local states
+  componentDidMount = () => {
+    this.props.getUserInformation(localStorage.auth_token).then(response => {
+      this.setState({ first_name: response.first_name, last_name: response.last_name });
+    });
+  };
+
   render() {
     return (
       <div>
         <h1 className="green-divider">
           {LOCALIZE.formatString(
             LOCALIZE.dashboard.title,
-            localStorage.first_name,
-            localStorage.last_name
+            this.state.first_name,
+            this.state.last_name
           )}
         </h1>
         <p>{LOCALIZE.dashboard.description}</p>
@@ -18,4 +39,15 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getUserInformation
+    },
+    dispatch
+  );
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Dashboard);
