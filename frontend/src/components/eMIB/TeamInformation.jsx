ba2a7144cecfe5fcad26dbc667a8e4267de9bffb
Francis Normand <francis.normand@canada.ca>
2019-06-18 09:11:07 -0400

    Added sections and regions in TeamInformation

--- frontend/src/components/eMIB/TeamInformation.jsx
+++ frontend/src/components/eMIB/TeamInformation.jsx
@@ -105,107 +105,123 @@ class TeamInformation extends Component {
         />
         <div>
           <h2>{LOCALIZE.emibTest.background.teamInformation.title}</h2>
-          <div>
-            <h3>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.title}</h3>
+          <section aria-labelledby="region1">
+            <h3 id="region1">
+              {LOCALIZE.emibTest.background.teamInformation.teamMembersSection.title}
+            </h3>
             <div>
-              <h4>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para1Title}</h4>
-              <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para1}</p>
-              <h4>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para2Title}</h4>
-              <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para2}</p>
-              <h4>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para3Title}</h4>
-              <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para3}</p>
+              <section aria-labelledby="region1-sub-region1">
+                <h4 id="region1-sub-region1">
+                  {LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para1Title}
+                </h4>
+                <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para1}</p>
+              </section>
+              <section aria-labelledby="region1-sub-region2">
+                <h4 id="region1-sub-region2">
+                  {LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para2Title}
+                </h4>
+                <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para2}</p>
+              </section>
+              <section aria-labelledby="region1-sub-region3">
+                <h4 id="region1-sub-region3">
+                  {LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para3Title}
+                </h4>
+                <p>{LOCALIZE.emibTest.background.teamInformation.teamMembersSection.para3}</p>
+              </section>
             </div>
-          </div>
-          <div>
-            <p>
-              {currentLanguage === LANGUAGES.english && (
-                <ImageZoom
-                  longdesc="#team-image-description"
-                  image={{
-                    src: emib_sample_test_example_team_chart_en,
-                    alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
-                    style: styles.testImage,
-                    className: "ie-zoom-cursor"
-                  }}
-                  zoomImage={{
-                    src: emib_sample_test_example_team_chart_en_zoomed,
-                    alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
-                    className: "ie-zoom-cursor"
-                  }}
-                />
-              )}
-              {currentLanguage === LANGUAGES.french && (
-                <ImageZoom
-                  longdesc="#team-image-description"
-                  image={{
-                    src: emib_sample_test_example_team_chart_fr,
-                    alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
-                    style: styles.testImage,
-                    className: "ie-zoom-cursor"
-                  }}
-                  zoomImage={{
-                    src: emib_sample_test_example_team_chart_fr_zoomed,
-                    alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
-                    className: "ie-zoom-cursor"
-                  }}
-                />
-              )}
-            </p>
-            <button
-              id="team-image-description"
-              onClick={this.openPopup}
-              className="btn btn-secondary"
-              style={styles.button}
-            >
-              {LOCALIZE.emibTest.background.teamInformation.teamChart.link}
-            </button>
-          </div>
-          <div>
-            <h3>{LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.title}</h3>
             <div>
-              <div>
-                <p>
-                  {
-                    LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
-                      .listDescription
-                  }
-                </p>
-                <ol>
-                  <li>
-                    <span className="font-weight-bold">
-                      {
-                        LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
-                          .item1Title
-                      }
-                    </span>
-                    {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item1}
-                  </li>
-                  <li>
-                    <span className="font-weight-bold">
-                      {
-                        LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
-                          .item2Title
-                      }
-                    </span>
-                    {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item2}
-                  </li>
-                  <li>
-                    <span className="font-weight-bold">
-                      {
-                        LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
-                          .item3Title
-                      }
-                    </span>
-                    {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item3}
-                  </li>
-                </ol>
-                <h4>
+              <p>
+                {currentLanguage === LANGUAGES.english && (
+                  <ImageZoom
+                    longdesc="#team-image-description"
+                    image={{
+                      src: emib_sample_test_example_team_chart_en,
+                      alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
+                      style: styles.testImage,
+                      className: "ie-zoom-cursor"
+                    }}
+                    zoomImage={{
+                      src: emib_sample_test_example_team_chart_en_zoomed,
+                      alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
+                      className: "ie-zoom-cursor"
+                    }}
+                  />
+                )}
+                {currentLanguage === LANGUAGES.french && (
+                  <ImageZoom
+                    longdesc="#team-image-description"
+                    image={{
+                      src: emib_sample_test_example_team_chart_fr,
+                      alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
+                      style: styles.testImage,
+                      className: "ie-zoom-cursor"
+                    }}
+                    zoomImage={{
+                      src: emib_sample_test_example_team_chart_fr_zoomed,
+                      alt: LOCALIZE.emibTest.background.teamInformation.teamChart.desciption,
+                      className: "ie-zoom-cursor"
+                    }}
+                  />
+                )}
+              </p>
+              <button
+                id="team-image-description"
+                onClick={this.openPopup}
+                className="btn btn-secondary"
+                style={styles.button}
+              >
+                {LOCALIZE.emibTest.background.teamInformation.teamChart.link}
+              </button>
+            </div>
+          </section>
+          <section aria-labelledby="region2">
+            <h3 id="region2">
+              {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.title}
+            </h3>
+            <div>
+              <p>
+                {
+                  LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
+                    .listDescription
+                }
+              </p>
+              <ol>
+                <li>
+                  <span className="font-weight-bold">
+                    {
+                      LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
+                        .item1Title
+                    }
+                  </span>
+                  {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item1}
+                </li>
+                <li>
+                  <span className="font-weight-bold">
+                    {
+                      LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
+                        .item2Title
+                    }
+                  </span>
+                  {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item2}
+                </li>
+                <li>
+                  <span className="font-weight-bold">
+                    {
+                      LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection
+                        .item3Title
+                    }
+                  </span>
+                  {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.item3}
+                </li>
+              </ol>
+              <section aria-labelledby="region2-sub-region1">
+                <h4 id="region2-sub-region1">
                   {LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.para1Title}
                 </h4>
                 <p>{LOCALIZE.emibTest.background.teamInformation.responsibilitiesSection.para1}</p>
-              </div>
+              </section>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     );
