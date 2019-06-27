907fe0a023dba409c27ff290aae10167a8ce7505
Francis Normand <francis.normand@canada.ca>
2019-06-27 08:46:56 -0400

    Loading the background information markdown content from the database

--- frontend/src/components/eMIB/BackgroundInformation.jsx
+++ frontend/src/components/eMIB/BackgroundInformation.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import ReactMarkdown from "react-markdown";
-import markdown_en from "./sample_test_markdown/BackgroundInformation_en.md";
-import markdown_fr from "./sample_test_markdown/BackgroundInformation_fr.md";
 import { LANGUAGES } from "../commons/Translation";
+import { getTestQuestions } from "../../modules/LoadTestContentRedux";
+import { TEST_DEFINITION } from "../../testDefinition";
 
 class BackgroundInformation extends Component {
+  static propTypes = {
+    // Provided by Redux
+    getTestQuestions: PropTypes.func
+  };
+
   state = {
     markdown_en: "",
     markdown_fr: ""
   };
 
-  // loads the markdown files (english and french versions)
+  // loads the markdown content (english and french versions)
   componentWillMount = () => {
-    fetch(markdown_en)
-      .then(response => response.text())
-      .then(text => this.setState({ markdown_en: text }));
-    fetch(markdown_fr)
-      .then(response => response.text())
-      .then(text => this.setState({ markdown_fr: text }));
+    this.props.getTestQuestions(TEST_DEFINITION.emib.sampleTest).then(response => {
+      // saving the background information markdown content in local states
+      this.setState({
+        markdown_en: response.background.en.background[0].markdown[0].text,
+        markdown_fr: response.background.fr.background[0].markdown[0].text
+      });
+    });
   };
 
   render() {
@@ -41,7 +49,15 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getTestQuestions
+    },
+    dispatch
+  );
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(BackgroundInformation);
