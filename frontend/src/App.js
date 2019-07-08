c13e90e702ec439070d9bc58bedfdf9afa4d0fbb
Michael Cherry <zorackiii@gmail.com>
2019-07-08 09:45:15 -0400

    Adding check to see if auth_token is undefined; if so, skip the verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,6 +51,10 @@ class App extends Component {
     // getting the authentication token from the local storage
     const auth_token = localStorage.auth_token;
 
+    if (auth_token === undefined) {
+      return;
+    }
+
     // checks if the token is still valid
     fetch("/api/auth/jwt/verify_token/", {
       method: "POST",
