bdc53adc95d87ad7469ef735078ec3afa2ad5f6e
Michael Cherry <zorackiii@gmail.com>
2019-07-09 12:28:39 -0400

    Dropping tests for API that will be abolished

--- backend/tests/test_retrieve_test_view.py
+++ backend/tests/test_retrieve_test_view.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from views.retrieve_test_view import (
     retrieve_json_from_name_date,
     TEST_META_DATA,
-    TEST_INSTRUCTIONS,
     TEST_QUESTIONS,
 )
 
@@ -72,31 +71,6 @@ class RetrieveMetaTest(TestCase):
         time = timezone.datetime.strptime("01/01/1500", "%d/%m/%Y").date()
         time = timezone.now() + timezone.timedelta(days=-1)
         real_json = retrieve_json_from_name_date("emibSampleTest", time, TEST_META_DATA)
-        expected_json = {"error", "no test item found"}
-        self.assertEqual(real_json, expected_json)
-
-
-class RetrievePreTest(TestCase):
-    def test_get_real_sample(self):
-        real_json = retrieve_json_from_name_date(
-            "emibSampleTest", timezone.now(), TEST_INSTRUCTIONS
-        )
-        expected_json = {"test_internal_name": "emibSampleTest", "instructions": []}
-        self.assertEqual(real_json, expected_json)
-
-    def test_get_nonexistant_test(self):
-        real_json = retrieve_json_from_name_date(
-            "IAmNotARealTest", timezone.now(), TEST_INSTRUCTIONS
-        )
-        expected_json = {"error", "no test with the given test_name"}
-        self.assertEqual(real_json, expected_json)
-
-    def test_get_test_before(self):
-        time = timezone.datetime.strptime("01/01/1500", "%d/%m/%Y").date()
-        time = timezone.now() + timezone.timedelta(days=-1)
-        real_json = retrieve_json_from_name_date(
-            "emibSampleTest", time, TEST_INSTRUCTIONS
-        )
         expected_json = {"error", "no test item found"}
         self.assertEqual(real_json, expected_json)
 
