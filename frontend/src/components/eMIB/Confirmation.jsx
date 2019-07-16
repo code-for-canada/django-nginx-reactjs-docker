9ffc2917894b3995f3cdb460ae0cd1ad04b47b4f
Francis Normand <francis.normand@canada.ca>
2019-07-16 14:56:20 -0400

    Added focusing action to submission confirmed content on COnfirmation page load

--- frontend/src/components/eMIB/Confirmation.jsx
+++ frontend/src/components/eMIB/Confirmation.jsx
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 import LOCALIZE from "../../text_resources";
 
 class Confirmation extends Component {
+  componentDidMount = () => {
+    // focusing on your test has been submitted content section on page load
+    document.getElementById("test-submission-content").focus();
+  };
   // TODO add survey link.
   render() {
     return (
-      <div>
-        <h1 className="green-divider">
-          {LOCALIZE.emibTest.confirmationPage.submissionConfirmedTitle}
-        </h1>
-        <p style={{ fontSize: 24 }}>
-          {LOCALIZE.formatString(
-            LOCALIZE.emibTest.confirmationPage.feedbackSurvey,
-            <a href="https://www.canada.ca/home.html" target="_blank" rel="noopener noreferrer">
-              {LOCALIZE.emibTest.confirmationPage.optionalSurvey}
-            </a>
-          )}
-        </p>
-        <p>
-          {LOCALIZE.formatString(
-            LOCALIZE.emibTest.confirmationPage.logout,
-            <a href="mailto:cfp.cpp-ppc.psc@canada.ca">cfp.cpp-ppc.psc@canada.ca</a>
-          )}
-        </p>
-        <p>{LOCALIZE.emibTest.confirmationPage.thankYou}</p>
-      </div>
+      <section aria-labelledby="test-submission-content">
+        <div id="test-submission-content" tabIndex={0}>
+          <h1 className="green-divider">
+            {LOCALIZE.emibTest.confirmationPage.submissionConfirmedTitle}
+          </h1>
+          <p style={{ fontSize: 24 }}>
+            {LOCALIZE.formatString(
+              LOCALIZE.emibTest.confirmationPage.feedbackSurvey,
+              <a href="https://www.canada.ca/home.html" target="_blank" rel="noopener noreferrer">
+                {LOCALIZE.emibTest.confirmationPage.optionalSurvey}
+              </a>
+            )}
+          </p>
+          <p>
+            {LOCALIZE.formatString(
+              LOCALIZE.emibTest.confirmationPage.logout,
+              <a href="mailto:cfp.cpp-ppc.psc@canada.ca">cfp.cpp-ppc.psc@canada.ca</a>
+            )}
+          </p>
+          <p>{LOCALIZE.emibTest.confirmationPage.thankYou}</p>
+        </div>
+      </section>
     );
   }
 }
